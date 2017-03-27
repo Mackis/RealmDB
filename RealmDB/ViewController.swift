4f01aaee0e855c2281fe96b21d68a704c6ad80a2
Malcolm Kumwenda <malcolm@byteorbit.com>
2017-03-27 12:56:29 +0200

    Initial commit

---
+++ RealmDB/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  RealmDB
+//
+//  Created by Malcolm Kumwenda on 2017/03/27.
+//  Copyright © 2017 ByteOrbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "it works"
+        cell.detailTextLabel?.text = "on going"
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+}
