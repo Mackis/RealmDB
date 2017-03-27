f79a9c708840354661a7bc1aa76be71f10520787
Malcolm Kumwenda <malcolm@byteorbit.com>
2017-03-27 14:27:38 +0200

    Completed add, delete and update

---
+++ RealmDB/TodoItem.swift
@@ -0,0 +1,14 @@
+//
+//  TodoItem.swift
+//  RealmDB
+//
+//  Created by Malcolm Kumwenda on 2017/03/27.
+//  Copyright © 2017 ByteOrbit. All rights reserved.
+//
+
+import RealmSwift
+
+class TodoItem: Object {
+    dynamic var detail = ""
+    dynamic var status = 0
+}
